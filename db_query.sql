b68486e2f5635e1ae46fa3decf0a808c0694d6d4
EmreCandan0 <115104572+EmreCandan0@users.noreply.github.com>
2025-06-26 11:23:39 +0300

    Create db_query.sql

---
+++ db_query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tiff_metadata (
+    id SERIAL PRIMARY KEY,
+    filename TEXT NOT NULL,
+    upload_time TIMESTAMP NOT NULL,
+    epsg INTEGER,
+    value TEXT,
+    geom geometry(POLYGON, 4326),  -- 4326 is WGS84 (latitude/longitude)
+    source_path TEXT,
+    file_size FLOAT,
+    band_type TEXT
+);
